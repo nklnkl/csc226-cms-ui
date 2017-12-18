177bb4852b6d3712f436274e68075bcadda5d689
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-18 00:33:39 -0500

    Add route view to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AccountService } from './account.service';
 import { SessionService } from './session.service';
 import { BlogPostService } from './blog-post.service';
 import { CommentService } from './comment.service';
+import { AppRoutingModule } from './app-routing.module';
+import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, AppRoutingModule
   ],
   providers: [AccountService, SessionService, BlogPostService, CommentService],
   bootstrap: [AppComponent]
